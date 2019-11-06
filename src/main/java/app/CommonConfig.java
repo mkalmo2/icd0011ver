693a7ae096999c87d958e68e4a7a6f7378d11c02
Märt Kalmo <mart.kalmo@gmail.com>
2019-11-06 09:33:17 +0200

    refactored config

--- src/main/java/app/CommonConfig.java
+++ src/main/java/app/CommonConfig.java
@@ -18,7 +18,6 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackages = { "app" })
 @PropertySource("classpath:/application.properties")
 public class CommonConfig {
 
--- src/main/java/app/invoice/InvoiceConfig.java
+++ src/main/java/app/invoice/InvoiceConfig.java
@@ -2,6 +2,7 @@ package app.invoice;
 
 import app.CommonConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
 @Import(CommonConfig.class)
+@ComponentScan(basePackages = { "app.invoice" })
 public class InvoiceConfig {
 
     @Bean
